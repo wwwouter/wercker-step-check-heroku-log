a357355f6daad585a1143624ad29218bfcc659c6
wwwouter <wouter@wercker.com>
2013-07-11 16:56:29 +0200

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 wercker-step-check-heroku-log
 =============================
+
+
+if you're using the heroku-deploy step (this step is injected if a Heroku deploy target is used)
+you don't have to specify your api key and app name twice
+just use the $HEROKU_KEY and $HEROKU_APP_NAME vars
+
---
+++ run.sh
@@ -0,0 +1,35 @@
+if [ ! -n "$WERCKER_CHECK_HEROKU_LOG_API_KEY" ]; then
+  error 'Please specify api-key property'
+  exit 1
+fi
+
+if [ ! -n "$WERCKER_CHECK_HEROKU_LOG_APP_NAME" ]; then
+  error 'Please specify app-name property'
+  exit 1
+fi
+
+cd $WERCKER_STEP_ROOT
+
+export CHECK_STATUS_CODE=`curl -u :$WERCKER_CHECK_HEROKU_LOG_API_KEY https://api.heroku.com/apps --write-out %{http_code} --silent --output /dev/null`
+
+if [[ $CHECK_STATUS_CODE -ne "200" ]]; then
+  error 'Cannot connect to Heroku, please verify API key.'
+  return 1
+fi
+
+curl -H "Accept: application/json" \
+  -u :$WERCKER_CHECK_HEROKU_LOG_API_KEY \
+  -X GET https://api.heroku.com/apps/$WERCKER_CHECK_HEROKU_LOG_APP_NAME/logs\?logplex\=true > logplex-url
+
+export LOGPLEX_URL=`cat logplex-url`
+
+curl $LOGPLEX_URL > logplex-result
+
+grep "State changed from starting to crashed" logplex-result > logplex-filtered
+
+export LOGPLEX_FILTERED="`cat logplex-filtered`"
+
+if [[  -n $LOGPLEX_FILTERED ]]; then
+  warn "Crashed message found"
+  return 1
+fi
